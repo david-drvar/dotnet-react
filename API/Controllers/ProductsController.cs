3e0513555622a183f37f41eaa15b1d394683cf19
David Drvar <david.drvar.bogdanovic@gmail.com>
2022-06-17 13:36:29 +0200

    API basic setup

---
+++ API/Controllers/ProductsController.cs
@@ -0,0 +1,35 @@
+﻿using API.Data;
+using API.Entities;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        private readonly StoreContext context;
+
+        public ProductsController(StoreContext context)
+        {
+            this.context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<Product>> GetProducts()
+        {
+            var products = await context.Products.ToListAsync();
+            return Ok(products);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Product>> GetProduct(int id)
+        {
+            return await context.Products.FindAsync(id);
+        }
+    }
+}
---
+++ API/Data/DbInitializer.cs
@@ -0,0 +1,51 @@
+﻿using API.Entities;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace API.Data
+{
+    public static class DbInitializer
+    {
+        public static void Initialize(StoreContext storeContext)
+        {
+            if (storeContext.Products.Any()) return;
+
+            var products = new List<Product>
+            {
+                new Product
+                {
+                    Name = "Angular Speedster Board 2000",
+                    Description = "Lorem ipsum",
+                    Price = 2000,
+                    PictureUrl = "/images/products/sb-ang1.png",
+                    Type = "Boots",
+                    Brand = "Angular",
+                    QuantityInStock = 10
+                },
+                new Product
+                {
+                    Name = "iPhone 13 Pro Max",
+                    Description = "Lorem ipsum",
+                    Price = 2100230,
+                    PictureUrl = "/images/products/sb-ang1.png",
+                    Type = "Phone",
+                    Brand = "Apple",
+                    QuantityInStock = 102
+                },
+                new Product
+                {
+                    Name = "iPod",
+                    Description = "Lorem ipsum",
+                    Price = 99,
+                    PictureUrl = "/images/products/sb-ang1.png",
+                    Type = "Music",
+                    Brand = "Apple",
+                    QuantityInStock = 1
+                }
+            };
+
+            storeContext.Products.AddRange(products);
+            storeContext.SaveChanges();
+        }
+    }
+}
