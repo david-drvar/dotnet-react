5f0e25d45f659011a36ed0df5114d089253bf856
David Drvar <david.drvar.bogdanovic@gmail.com>
2022-06-20 10:35:25 +0200

    client - app bar, grid, card item and styling

--- client/src/features/catalog/Catalog.tsx
+++ client/src/features/catalog/Catalog.tsx
@@ -1,6 +1,7 @@
 import { Avatar, Button, List, ListItem, ListItemAvatar, ListItemText } from "@mui/material";
 import { Fragment } from "react";
 import { Product } from "../../app/models/product";
+import ProductList from "./ProductList";
 
 interface Props {
     products: Product[];
@@ -8,21 +9,9 @@ interface Props {
 }
 
 export default function Catalog({products, addProduct}: Props) {
-
     return (
         <>
-            <List>
-                {products.map((product) => (
-                    <ListItem key={product.id}>
-                        <ListItemAvatar>
-                            <Avatar src={product.pictureUrl} />
-                        </ListItemAvatar>
-                        <ListItemText>
-                        {product.name} - {product.price}
-                        </ListItemText>
-                        </ListItem>
-                ))}
-            </List>
+            <ProductList products={products} />
             <Button variant="contained" onClick={addProduct}>Add product</Button>
         </>
     )
